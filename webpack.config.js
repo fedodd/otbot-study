bad9215b09d123fc4a0a442c3dc557de0f574fc1
fedodd <fedodd.s@gmail.com>
2018-04-23 16:59:48 +0300

    config project, and start front

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
+
+function generateHtmlPlugins(templateDir) {
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map((item) => {
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      inject: false
+    });
+  });
+}
+
+const htmlPlugins = generateHtmlPlugins('./src/html/views');
+
+module.exports = {
+  entry: [
+    './src/js/index.js',
+    './src/styles/common.css'
+  ],
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src/js'),
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: 'env'
+        }
+      }
+    },
+    {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      })
+    },
+      /*       use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+      })
+    }, */
+    {
+      test: /\.html$/,
+      include: path.resolve(__dirname, 'src/html/includes'),
+      use: ['raw-loader']
+    }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: './styles/styles.css',
+      allChunks: true
+    }),
+    new CopyWebpackPlugin([{
+      from: './src/fonts',
+      to: './fonts'
+    },
+    {
+      from: './src/img',
+      to: './img'
+    }
+    ])
+  ].concat(htmlPlugins)
+};
