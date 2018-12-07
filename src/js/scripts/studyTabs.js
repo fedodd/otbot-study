732181f153f3305240b1e6103d5e3a97f0125a9f
fedodd <fedodd.s@gmail.com>
2018-12-07 15:51:47 +0300

    text edit

--- src/js/scripts/studyTabs.js
+++ src/js/scripts/studyTabs.js
@@ -1,13 +1,18 @@
 const studyTabs = document.querySelectorAll('.subNav-input');
 const studyContents = document.querySelectorAll('.studyContent-item');
 
+/* сделаем первый таб активным */
+studyContents[0].classList.add('is__active');
+studyTabs[0].checked = true;
 
+/* переключатель */
 const toggleFunc = (studyContents, index) => {
 	[...studyContents].map(
 		studyContent => studyContent.classList.remove('is__active'));
 	[...studyContents][index].classList.add('is__active');
 };
 
+/* listener на кнопки табов */
 [...studyTabs].map((studyTab) => {
 	const currIndex = [...studyTabs].indexOf(studyTab);
 	studyTab.addEventListener('click', function(){toggleFunc(studyContents, currIndex)});
