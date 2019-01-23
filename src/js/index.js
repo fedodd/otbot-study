79dd477201bdb6ef5e585e29182264baa110a668
fedodd <fedodd.s@gmail.com>
2019-01-23 16:10:15 +0300

    add collection point to PB

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,17 @@ import mytoggle from './scripts/toggle';
  */
 import studyTabs from './scripts/studyTabs';
 /* import OTChart from './scripts/OTChart'; */
+
+
+const toggleBlocks = document.querySelectorAll('.imageList.is__small.is__toggle');
+const toggleActivators = document.querySelectorAll('.imageList-title.is__toggle');
+
+const toggleFunc = (toggleBlocks, index) => {
+  [...toggleBlocks][index].classList.toggle('is__active');
+};
+
+/* listener на кнопки табов */
+[...toggleActivators].map((Activator) => {
+  const currIndex = [...toggleActivators].indexOf(Activator);
+  Activator.addEventListener('click', function () { toggleFunc(toggleBlocks, currIndex) });
+});
